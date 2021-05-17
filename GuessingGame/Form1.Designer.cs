8ad24c3dcef91ef2da2d7cf02e255be483683819
trenmcki071 <trenmcki071@outlook.com>
2021-05-17 18:39:47 -0400

    Add project files.

---
+++ GuessingGame/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+namespace GuessingGame
+{
+    partial class guessingGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.guessButton = new System.Windows.Forms.Button();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.finalText = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(96, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(166, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Guessing Game";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(99, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(164, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Pick a number between 1 and 10";
+            // 
+            // guessButton
+            // 
+            this.guessButton.Location = new System.Drawing.Point(136, 168);
+            this.guessButton.Name = "guessButton";
+            this.guessButton.Size = new System.Drawing.Size(75, 31);
+            this.guessButton.TabIndex = 2;
+            this.guessButton.Text = "Guess";
+            this.guessButton.UseVisualStyleBackColor = true;
+            this.guessButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Location = new System.Drawing.Point(125, 127);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(100, 20);
+            this.textBoxInput.TabIndex = 3;
+            // 
+            // finalText
+            // 
+            this.finalText.AutoSize = true;
+            this.finalText.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.finalText.Location = new System.Drawing.Point(110, 226);
+            this.finalText.Name = "finalText";
+            this.finalText.Size = new System.Drawing.Size(0, 25);
+            this.finalText.TabIndex = 4;
+            // 
+            // guessingGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(365, 279);
+            this.Controls.Add(this.finalText);
+            this.Controls.Add(this.textBoxInput);
+            this.Controls.Add(this.guessButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "guessingGame";
+            this.Text = "Guessing Game";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button guessButton;
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.Label finalText;
+    }
+}
+
---
+++ GuessingGame/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GuessingGame
+{
+    public partial class guessingGame : Form
+    {
+        public guessingGame()
+        {
+            InitializeComponent();
+
+            randomNumber = rnd.Next(1, 11);
+        }
+        Random rnd = new Random();
+        int textBoxNumber;
+        int randomNumber;
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                textBoxNumber = Int32.Parse(textBoxInput.Text);
+                if(textBoxNumber == randomNumber)
+                {
+                    finalText.Text = $"{textBoxNumber} is the number!";
+                }
+                else if(textBoxNumber < randomNumber)
+                {
+                    finalText.Text = $"{textBoxNumber} is too low";
+                }
+                else if(textBoxNumber > randomNumber)
+                {
+                    finalText.Text = $"{textBoxNumber} is too high";
+                }
+                else if(textBoxNumber < 10)
+                {
+                    finalText.Text = $"A number between 1 and 10 please";
+                }
+            }
+            catch
+            {
+                finalText.Text = "Enter a number value";
+            }
+        }
+    }
+}
